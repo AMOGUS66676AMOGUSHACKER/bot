f1d9e3d487ce52ea514c2a9929cee724b6bb40ad
wot <144359407+AMOGUS66676AMOGUSHACKER@users.noreply.github.com>
2025-02-09 20:51:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ def run():
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 def keep_alive():
+    pass  # Щось має бути всередині функції
     thread = Thread(target=run)
     thread.start()
 
