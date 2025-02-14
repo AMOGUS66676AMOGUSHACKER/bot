e005e22698d5c6deb5c0a6caee78c924cc80ea76
wot <144359407+AMOGUS66676AMOGUSHACKER@users.noreply.github.com>
2025-02-14 17:32:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -140,13 +140,14 @@ async def start(message: types.Message):
 if message.from_user.id == ID:
     await message.answer('Добро пожаловать!', reply_markup=menu)  # Адмінське меню
 else:
-    await message.answer('Добро пожаловать!', reply_markup=panel)  # Меню для юзера
-    if not result:
-            cursor.execute('INSERT INTO users (user_id) VALUES (?)', (message.from_user.id,))
-            if message.from_user.username != None:
-                cursor.execute(f'UPDATE users SET nick = ? WHERE user_id = ?',
-                               ('@' + message.from_user.username, message.from_user.id,))
-            conn.commit()
+    cursor.execute('SELECT id FROM users WHERE user_id = ?', (message.from_user.id,))
+    result = cursor.fetchall()
+
+    if not result:  # ⬅ Цей рядок має бути з відступом 4 пробіли
+        cursor.execute('INSERT INTO users (user_id) VALUES (?)', (message.from_user.id,))
+        conn.commit()
+
+    await message.answer('Добро пожаловать!', reply_markup=panel)  # Меню для юзерів
         cursor.execute('SELECT block FROM users WHERE user_id = ?', (message.from_user.id,))
         result = cursor.fetchall()
         if result[0][0] != 1:
